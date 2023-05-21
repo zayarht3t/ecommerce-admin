7e5bb2b8cb90a97ce792a1f0ca911c42bb6c4a5f
zayarht3t <zayy4699@gmail.com>
2023-05-21 09:35:09 -0400

    add order table

--- pages/orders.js
+++ pages/orders.js
@@ -1,83 +1,70 @@
 import Layout from '@/components/Layout'
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const Orders = () => {
+
+    const [orders, setOrders] = useState([]);
+
+    useEffect( ()=>{
+        const fetchData = async () => {
+        const response = await axios.get('api/orders');
+        setOrders(response.data);            
+        }
+
+        fetchData();
+
+    },[])
   return (
     <Layout>
         
-<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+<div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                <th scope="col" class="px-6 py-3">
-                    Product name
+                <th scope="col" className="px-6 py-3">
+                    Date
                 </th>
-                <th scope="col" class="px-6 py-3">
-                    Color
+                <th scope="col" className="px-6 py-3">
+                    Recipient
                 </th>
-                <th scope="col" class="px-6 py-3">
-                    Category
+                <th scope="col" className="px-6 py-3">
+                    Product
                 </th>
-                <th scope="col" class="px-6 py-3">
-                    Price
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    <span class="sr-only">Edit</span>
+                <th scope="col" className="px-6 py-3">
+                    <span className="sr-only">Edit</span>
                 </th>
             </tr>
         </thead>
         <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Apple MacBook Pro 17"
-                </th>
-                <td class="px-6 py-4">
-                    Silver
-                </td>
-                <td class="px-6 py-4">
-                    Laptop
-                </td>
-                <td class="px-6 py-4">
-                    $2999
-                </td>
-                <td class="px-6 py-4 text-right">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                </td>
-            </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Microsoft Surface Pro
-                </th>
-                <td class="px-6 py-4">
-                    White
-                </td>
-                <td class="px-6 py-4">
-                    Laptop PC
-                </td>
-                <td class="px-6 py-4">
-                    $1999
-                </td>
-                <td class="px-6 py-4 text-right">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                </td>
-            </tr>
-            <tr class="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Magic Mouse 2
-                </th>
-                <td class="px-6 py-4">
-                    Black
-                </td>
-                <td class="px-6 py-4">
-                    Accessories
-                </td>
-                <td class="px-6 py-4">
-                    $99
-                </td>
-                <td class="px-6 py-4 text-right">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                </td>
-            </tr>
+            {
+                orders.length > 0 &&
+                orders.map(order=>(
+                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {order.createdAt}
+                    </th>
+                    <td className="px-6 py-4">
+                        {order.name} {order.email}<br/>
+                        {order.city} {order.postalCode}<br/>
+                        {order.country}<br/>
+                        {order.street}
+                    </td>
+                    {
+                        order.line_items.length >0 &&
+                        order.line_items.map(item=>(
+                            <td className="px-6 py-4">
+                                {item.price_data.product_data.name} * {item.quantity}
+                            </td>
+                        ))
+                    }
+                    
+                    <td className="px-6 py-4 text-right">
+                        <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                    </td>
+                </tr>
+                ))
+            }
         </tbody>
     </table>
 </div>
